f223bbde7ae6e531ecb34959893b170650fab1f0
Jeremy Davis <jeremy.davis@sendgrid.com>
2014-06-30 14:47:12 -0600

    Added comments.

--- splunk-export.sh
+++ splunk-export.sh
@@ -20,13 +20,15 @@ echo "NOTE: This filename will be used to store the exported logs from Splunk. O
 read FILENAME
 clear
 
+## Display what we are about to do.
 echo "Your username: $USERNAME"
 echo "Your Query: $QUERY"
-
 echo "Performing Splunk Search. Please wait..."
 
+## Perform the search and export.
 curl  -k -u $USERNAME:$PASSWD --data-urlencode search="$QUERY" -d "output_mode=csv" https://splunk.sendgrid.net:8089/servicesNS/admin/search/search/jobs/export >> $FILENAME
 
+## If everything finished let the user know and exit clean.
 clear
 echo "Finished!! Please open $FILENAME to view the exported data."
 exit 0
